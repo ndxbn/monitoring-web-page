498ca6a214de5b8d9ab0642120c574f62ff99b0d
ndxbn <sitri.ndxbn@gmail.com>
2019-02-21 19:58:01 +0900

    fix: curl option mistake

--- sana_natori_twpf.sh
+++ sana_natori_twpf.sh
@@ -1,11 +1,11 @@
 #! /usr/bin/env bash
 set -eu
 
+base_path="${HOME}/storage/twpf.jp/sana_natori/"
 file_name="$(date -Ins)"
-base_path="./storage/twpf.jp/sana_natori/"
 file_path="${base_path}${file_name}"
 
 mkdir -p ${base_path}
 
-curl -q "http://twpf.jp/sana_natori" > ${file_path}
+curl -s "http://twpf.jp/sana_natori" > ${file_path}
 
